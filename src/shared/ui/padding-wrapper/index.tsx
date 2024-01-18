b60e61397b8f6fb3d1b484ea3ff309fcadc65ae4
Muhammadsalam Musaev <muhammadsalam@list.ru>
2024-01-18 16:14:48 +0300

    padding-wrapper user offset

--- src/shared/ui/padding-wrapper/index.tsx
+++ src/shared/ui/padding-wrapper/index.tsx
@@ -1,8 +1,12 @@
 import { FC, ReactNode } from "react";
 
-export const PaddingWrapper: FC<{ children: ReactNode }> = ({ children }) => {
+export const PaddingWrapper: FC<{
+    ptb?: number;
+    plr?: number;
+    children: ReactNode;
+}> = ({ ptb = 4, plr = 16, children }) => {
     const styles = {
-        padding: "4px 16px",
+        padding: `${ptb}px ${plr}px`,
     };
 
     return <div style={styles}>{children}</div>;
