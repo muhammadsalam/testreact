a3389ec9b6d4acd68712810dd27e8dfa27cb940b
Muhammadsalam Musaev <muhammadsalam@list.ru>
2024-01-20 15:38:05 +0300

    Strategy page

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { ProfilePage } from "pages/profile";
 import { ConfigurePage } from "pages/configure";
+import { App } from "app/App";
+import { StrategyPage } from "pages/strategy";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<ConfigurePage />);
+ReactDOM.createRoot(document.getElementById("root")!).render(<StrategyPage />);
---
+++ src/pages/strategy/ui/index.tsx
@@ -0,0 +1,92 @@
+import { FC, useRef, useState } from "react";
+import styles from "./style.module.scss";
+import { Cell, Dropdown, FlexWrapper, PaddingWrapper } from "shared/ui";
+import clsx from "clsx";
+import ArrowBottomIcon from "../../../assets/icons/arrow-bottom.svg?react";
+import { useOutsideClick } from "shared/lib";
+
+export const StrategyPage: FC = () => {
+    const tabs = ["Manually", "By indicator"];
+    const [activeTab, setActiveTab] = useState<(typeof tabs)[number]>(
+        tabs[0].toLowerCase()
+    );
+
+    const [isDropdownActive, setIsDropdownActive] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    const handleDropdownActive = () => {
+        setIsDropdownActive(true);
+    };
+    useOutsideClick(dropdownRef, () => {
+        setIsDropdownActive(false);
+    });
+
+    return (
+        <>
+            <div className={styles.container}>
+                <div className={styles.top}>
+                    <p className={styles.top_title}>Strategy</p>
+                    <p className={styles.top_subtitle}>
+                        Select the type of trading for detailed bot setup
+                    </p>
+                </div>
+
+                <div className={styles.tabs}>
+                    {tabs.map((tab, index) => (
+                        <button
+                            key={index}
+                            className={clsx(
+                                styles.tabs_button,
+                                activeTab === tab.toLowerCase() &&
+                                    styles.tabs_button__active
+                            )}
+                            onClick={() => setActiveTab(tab.toLowerCase())}
+                        >
+                            {tab}
+                        </button>
+                    ))}
+                </div>
+
+                <Cell description="Min 5.03 USDT / 0.00021 BTC">
+                    <div className={styles.list_item}>
+                        <p className={styles.list_item_title}>Input type</p>
+                        <Dropdown
+                            items={[
+                                "From the first order",
+                                "title-1",
+                                "title-2",
+                            ]}
+                        />
+                    </div>
+                    <div className={styles.list_item}>
+                        <p className={styles.list_item_title}>
+                            Volume of the first order
+                        </p>
+                        <span className={styles.list_item_span}>77</span>
+                    </div>
+                </Cell>
+
+                <Cell>
+                    <div className={styles.list_item}>
+                        <p className={styles.list_item_title}>
+                            Type of the first order
+                        </p>
+                        <Dropdown
+                            items={["Limit order", "title-1", "title-2"]}
+                        />
+                    </div>
+                    <div className={styles.list_item}>
+                        <p className={styles.list_item_title}>
+                            Price for a limit order
+                        </p>
+                        <span className={styles.list_item_span}>1000</span>
+                    </div>
+                </Cell>
+            </div>
+            <div className={styles.bottom}>
+                <button className={styles.btn__primary}>
+                    Next to step 3 / 6
+                </button>
+            </div>
+        </>
+    );
+};
