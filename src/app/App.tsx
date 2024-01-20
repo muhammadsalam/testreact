78c774bfc68e3540e5b5036cc6e9f4392f528c41
Muhammadsalam Musaev <muhammadsalam@list.ru>
2024-01-20 12:59:14 +0300

    cell components in App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,7 @@ import styles from "./App.module.scss";
 import { clsx } from "clsx";
 import ArrowRightIcon from "../assets/icons/arrow.svg?react";
 import PlusSVG from "../assets/icons/plus.svg?react";
-import { FlexWrapper, Range, Switcher } from "shared/ui";
-import { PaddingWrapper } from "shared/ui/padding-wrapper";
+import { Cell, FlexWrapper, Range, Switcher } from "shared/ui";
 
 export const App: FC = () => {
     const tabs = ["Insurance orders", "Stop Loss"];
@@ -71,140 +70,95 @@ export const App: FC = () => {
                 ))}
             </div>
 
-            <div className={styles.navButton}>
-                <PaddingWrapper>
-                    <FlexWrapper>
-                        <p className={styles.navButton_text}>
-                            Type of insurance order
-                        </p>
-                    </FlexWrapper>
-                </PaddingWrapper>
+            <Cell title="Type of insurance order">
                 <button className={styles.navButton_button}>
                     Market Order
                     <ArrowRightIcon className={styles.navButton_icon} />
                 </button>
-            </div>
+            </Cell>
 
-            <div className={styles.blockList}>
-                <div className={styles.blockList_content}>
-                    <div className={styles.blockList_item}>
-                        <FlexWrapper>
-                            <p className={styles.blockList_item_title}>
-                                Limit of insurance orders
-                            </p>
-                            <span className={styles.blockList_item_span}>
-                                10
-                            </span>
-                        </FlexWrapper>
-                    </div>
-                    <div className={styles.blockList_item}>
-                        <FlexWrapper>
-                            <p className={styles.blockList_item_title}>
-                                Step of insurance orders, %
-                            </p>
-                            <span className={styles.blockList_item_span}>
-                                2
-                            </span>
-                        </FlexWrapper>
-                    </div>
-                </div>
-            </div>
-
-            <div className={styles.blockList}>
-                <div className={styles.blockList_content}>
-                    <div className={clsx(styles.blockList_item, styles.switch)}>
-                        <FlexWrapper>
-                            <p className={styles.switch_title}>Martingale</p>
-                            <Switcher />
-                        </FlexWrapper>
-                    </div>
-                    <div className={styles.blockList_item}>
-                        <Range min={0.1} max={5} currValue={3.5} />
-                    </div>
-                </div>
-                <p className={styles.blockList_description}>
-                    The Martingale system is a system in which the dollar value
-                    of trades increases after losses, or position size increases
-                    with a smaller portfolio size.
-                </p>
-            </div>
-
-            <div className={styles.blockList}>
-                <div className={styles.blockList_content}>
-                    <div className={clsx(styles.blockList_item, styles.switch)}>
-                        <FlexWrapper>
-                            <p className={styles.switch_title}>
-                                Dynamic price step CO
-                            </p>
-                            <Switcher />
-                        </FlexWrapper>
-                    </div>
-                    <div className={styles.blockList_item}>
-                        <Range min={1.0} max={5} currValue={2} />
-                    </div>
-                </div>
-                <p className={styles.blockList_description}>
-                    The price level of each next order changes according to the
-                    specified coefficient
-                </p>
-            </div>
-
-            <div className={styles.navButton}>
-                <PaddingWrapper>
+            <Cell>
+                <div className={styles.blockList_item}>
                     <FlexWrapper>
-                        <p className={styles.navButton_text}>Order type</p>
+                        <p className={styles.blockList_item_title}>
+                            Limit of insurance orders
+                        </p>
+                        <span className={styles.blockList_item_span}>10</span>
                     </FlexWrapper>
-                </PaddingWrapper>
+                </div>
+                <div className={styles.blockList_item}>
+                    <FlexWrapper>
+                        <p className={styles.blockList_item_title}>
+                            Step of insurance orders, %
+                        </p>
+                        <span className={styles.blockList_item_span}>2</span>
+                    </FlexWrapper>
+                </div>
+            </Cell>
+
+            <Cell description=" The Martingale system is a system in which the dollar value of trades increases after losses, or position size increases with a smaller portfolio size.">
+                <div className={clsx(styles.blockList_item, styles.switch)}>
+                    <FlexWrapper>
+                        <p className={styles.switch_title}>Martingale</p>
+                        <Switcher />
+                    </FlexWrapper>
+                </div>
+                <div className={styles.blockList_item}>
+                    <Range min={0.1} max={5} currValue={3.5} />
+                </div>
+            </Cell>
+
+            <Cell description="The price level of each next order changes according to the specified coefficient">
+                <div className={clsx(styles.blockList_item, styles.switch)}>
+                    <FlexWrapper>
+                        <p className={styles.switch_title}>
+                            Dynamic price step CO
+                        </p>
+                        <Switcher />
+                    </FlexWrapper>
+                </div>
+                <div className={styles.blockList_item}>
+                    <Range min={1.0} max={5} currValue={2} />
+                </div>
+            </Cell>
+
+            <Cell title="Order type">
                 <button className={styles.navButton_button}>
                     Limit Order
                     <ArrowRightIcon className={styles.navButton_icon} />
                 </button>
-            </div>
+            </Cell>
 
-            <div className={styles.blockList}>
-                <PaddingWrapper>
+            <Cell title="Remaining by volume">
+                <div className={styles.blockList_item}>
                     <FlexWrapper>
-                        <p className={styles.navButton_text}>
-                            Remaining by volume
-                        </p>
+                        <div className={styles.progressbar}>
+                            <span
+                                className={styles.progressbar_bar}
+                                style={{ width: "40%" }}
+                            ></span>
+                        </div>
+                        <span className={styles.blockList_item_span}>40%</span>
                     </FlexWrapper>
-                </PaddingWrapper>
-                <div className={styles.blockList_content}>
-                    <div className={styles.blockList_item}>
-                        <FlexWrapper>
-                            <div className={styles.progressbar}>
-                                <span
-                                    className={styles.progressbar_bar}
-                                    style={{ width: "40%" }}
-                                ></span>
-                            </div>
-                            <span className={styles.blockList_item_span}>
-                                40%
-                            </span>
-                        </FlexWrapper>
-                    </div>
                 </div>
-            </div>
+            </Cell>
 
-            <div className={styles.lists}>
-                {steps.map((step, index) => (
-                    <div className={styles.blockList}>
-                        <PaddingWrapper>
-                            <FlexWrapper>
-                                <p className={styles.navButton_text}>
-                                    Step {index + 1}
-                                </p>
-                                {index !== 0 && (
+            <Cell>
+                <div className={styles.lists}>
+                    {steps.map((step, index) => (
+                        <Cell
+                            title={`Step ${index + 1}`}
+                            topContextBtn={
+                                Boolean(index) && (
                                     <button
                                         className={styles.step_delete}
                                         onClick={() => handleStepDelete(index)}
                                     >
                                         Delete
                                     </button>
-                                )}
-                            </FlexWrapper>
-                        </PaddingWrapper>
-                        <div className={styles.blockList_content}>
+                                )
+                            }
+                        >
                             {step.map((item) => (
                                 <div className={styles.blockList_item}>
                                     <FlexWrapper>
@@ -225,14 +179,17 @@ export const App: FC = () => {
                                     </FlexWrapper>
                                 </div>
                             ))}
-                        </div>
-                    </div>
-                ))}
+                        </Cell>
+                    ))}
 
-                <button className={styles.lists_btn} onClick={handleStepAdd}>
-                    <PlusSVG className={styles.lists_btn_icon} /> Add a step
-                </button>
-            </div>
+                    <button
+                        className={styles.lists_btn}
+                        onClick={handleStepAdd}
+                    >
+                        <PlusSVG className={styles.lists_btn_icon} /> Add a step
+                    </button>
+                </div>
+            </Cell>
         </div>
     );
 };
