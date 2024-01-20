df6057d557153944f2b9e3018bc11e6d77e02716
Muhammadsalam Musaev <muhammadsalam@list.ru>
2024-01-20 12:26:53 +0300

    configure cells

--- src/pages/configure/ui/index.tsx
+++ src/pages/configure/ui/index.tsx
@@ -1,11 +1,8 @@
 import { FC } from "react";
 import styles from "./style.module.scss";
-import { FlexWrapper, PaddingWrapper, Switcher } from "shared/ui";
+import { Cell, Switcher } from "shared/ui";
 import ArrowRightIcon from "../../../assets/icons/arrow.svg?react";
-import BinanceIcon from "../../../assets/icons/binance.svg?react";
 import BtcusdtIcon from "../../../assets/icons/btcusdt.svg?react";
-import SolusdcIcon from "../../../assets/icons/solusdc.svg?react";
-import TwtusdtIcon from "../../../assets/icons/twtusdt.svg?react";
 import ChartIcon from "../../../assets/icons/chart.svg?react";
 
 export const ConfigurePage: FC = () => {
@@ -19,21 +16,11 @@ export const ConfigurePage: FC = () => {
                     </p>
                 </div>
 
-                <div>
-                    <PaddingWrapper>
-                        <FlexWrapper>
-                            <p className={styles.navButton_text}>bot name</p>
-                        </FlexWrapper>
-                    </PaddingWrapper>
+                <Cell title="bot name">
                     <div className={styles.block}>Bot 1</div>
-                </div>
+                </Cell>
 
-                <div className={styles.cell}>
-                    <PaddingWrapper>
-                        <FlexWrapper>
-                            <p className={styles.navButton_text}>bot name</p>
-                        </FlexWrapper>
-                    </PaddingWrapper>
+                <Cell title="pair" description="1 BTC = 26 280.25 ₮">
                     <button className={styles.navButton}>
                         <div className={styles.content}>
                             <BtcusdtIcon />
@@ -45,47 +32,29 @@ export const ConfigurePage: FC = () => {
                         </div>
                         <ArrowRightIcon className={styles.navButton_icon} />
                     </button>
+                </Cell>
 
-                    <PaddingWrapper className={styles.cell_description}>
-                        1 BTC = 26 280.25 ₮
-                    </PaddingWrapper>
-                </div>
-
-                <div className={styles.cell}>
-                    <PaddingWrapper>
-                        <FlexWrapper>
-                            <p className={styles.navButton_text}>trade</p>
-                        </FlexWrapper>
-                    </PaddingWrapper>
+                <Cell title="trade">
                     <div className={styles.block}>
                         <ChartIcon />
                         Long
                     </div>
-                </div>
+                </Cell>
 
-                <div className={styles.cell}>
-                    <PaddingWrapper>
-                        <FlexWrapper>
-                            <p className={styles.navButton_text}>
-                                additionally
-                            </p>
-                        </FlexWrapper>
-                    </PaddingWrapper>
-                    <div className={styles.list}>
-                        <div className={styles.list_item}>
-                            Strategy
-                            <Switcher />
-                        </div>
-                        <div className={styles.list_item}>
-                            Defends
-                            <Switcher />
-                        </div>
-                        <div className={styles.list_item}>
-                            Take Profit
-                            <Switcher />
-                        </div>
+                <Cell title="additionally">
+                    <div className={styles.list_item}>
+                        Strategy
+                        <Switcher />
                     </div>
-                </div>
+                    <div className={styles.list_item}>
+                        Defends
+                        <Switcher />
+                    </div>
+                    <div className={styles.list_item}>
+                        Take Profit
+                        <Switcher />
+                    </div>
+                </Cell>
             </div>
             <div className={styles.bottom}>
                 <button className={styles.btn__primary}>
