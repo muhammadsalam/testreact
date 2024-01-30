eee66192f2cc300b0cf6d6918a6b69469937301e
Muhammadsalam Musaev <muhammadsalam@list.ru>
2024-01-30 15:56:52 +0300

    defends validation

--- src/pages/create-bot/layouts/defends/ui/index.tsx
+++ src/pages/create-bot/layouts/defends/ui/index.tsx
@@ -20,7 +20,19 @@ export const DefendsLayout: FC = () => {
     ];
 
     const {
-        bot: { def_type, active_buy, active_def, active_tp },
+        addAlert,
+        otherStates: { def_mrt, def_step_mrt },
+        bot: {
+            def_type,
+            active_buy,
+            active_def,
+            active_tp,
+            io_count,
+            io_step,
+            io_mrt,
+            io_step_mrt,
+            stop_loss,
+        },
         setBot,
     } = useBot();
 
@@ -28,6 +40,46 @@ export const DefendsLayout: FC = () => {
     const handleTabChange = (tabId: "IO" | "SL") => {
         setActiveTab(tabId);
         setBot((prev) => ({ ...prev, def_type: tabId }));
+    };
+
+    const validation = (): boolean => {
+        if (def_type === "IO") {
+            if (+io_count < 1 || +io_count > 10) {
+                addAlert({
+                    title: "limit of insurance orders should be between 1 and 10",
+                });
+                return false;
+            }
+            if (+io_step < 1 || +io_step > 99) {
+                addAlert({
+                    title: "step of insurance orders should be between 1 and 99",
+                });
+                return false;
+            }
+            if (def_mrt && (+io_mrt < 1 || +io_mrt > 5)) {
+                addAlert({
+                    title: "martingale of insurance orders should be between 1 and 5",
+                });
+                return false;
+            }
+            if (def_step_mrt && (+io_step_mrt < 1 || +io_step_mrt > 5)) {
+                addAlert({
+                    title: "dynamic price of insurance orders should be between 1 and 5",
+                });
+                return false;
+            }
+        }
+
+        if (def_type === "SL") {
+            if (+stop_loss < 1 || +stop_loss > 99) {
+                addAlert({
+                    title: "stop loss should be between 1 and 99",
+                });
+                return false;
+            }
+        }
+
+        return true;
     };
 
     useEffect(() => {
@@ -41,8 +93,10 @@ export const DefendsLayout: FC = () => {
         tgApp.BackButton.onClick(backButtonHandler);
 
         const mainButtonHandler = () => {
-            if (active_tp) window.location.hash = "#4";
-            else window.location.hash = "#5";
+            if (validation()) {
+                if (active_tp) window.location.hash = "#4";
+                else window.location.hash = "#5";
+            }
         };
         tgApp.MainButton.onClick(mainButtonHandler);
 
@@ -53,7 +107,18 @@ export const DefendsLayout: FC = () => {
             tgApp.BackButton.offClick(backButtonHandler);
             tgApp.MainButton.offClick(mainButtonHandler);
         };
-    }, [active_buy, active_def, active_tp]);
+    }, [
+        active_buy,
+        active_def,
+        active_tp,
+        io_step,
+        io_mrt,
+        io_step_mrt,
+        def_mrt,
+        def_step_mrt,
+        io_count,
+        stop_loss,
+    ]);
 
     const render = () => {
         switch (activeTab) {
