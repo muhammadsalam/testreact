8bc404637f9c9ff80f0a1af043bab591cb10b723
Muhammadsalam Musaev <muhammadsalam@list.ru>
2024-02-07 16:17:15 +0300

    Главная с пустым массивом ботов и нет ключей

--- src/pages/profile/ui/index.tsx
+++ src/pages/profile/ui/index.tsx
@@ -8,10 +8,14 @@ import SolusdcIcon from "../../../assets/icons/solusdc.svg?react";
 import TwtusdtIcon from "../../../assets/icons/twtusdt.svg?react";
 import { Link } from "react-router-dom";
 import { tgApp } from "shared/lib";
+import { useSelector } from "react-redux";
+import clsx from "clsx";
+import PlusIcon from "assets/icons/plus.svg?react";
 
 export const ProfilePage: FC = () => {
     useEffect(() => {
         tgApp.MainButton.hide();
+        tgApp.BackButton.hide();
     }, []);
 
     // const [isFontsLoading, setIsFontsLoading] = useState({
@@ -37,88 +41,147 @@ export const ProfilePage: FC = () => {
     //     return <Loader />;
     // }
 
+    const userData = useSelector((state: any) => state.user.data);
+
     return (
         <div className={styles.container}>
             <div className={styles.top}>
                 <p className={styles.top_title}>Deposit</p>
                 <strong className={styles.top_strong}>
-                    15 194.35 <span>$</span>
+                    {userData.used_balance} <span>$</span>
                 </strong>
-                <span className={styles.top_sub}>+234.35</span>
+                {+userData.profit !== 0 && (
+                    <span
+                        className={clsx(styles.top_sub, {
+                            [styles.top_sub__red]: userData.profit < 0,
+                        })}
+                    >
+                        {userData.profit > 0 && "+"}
+                        {userData.profit} $
+                    </span>
+                )}
             </div>
 
             <Link to="/createbot" style={{ display: "contents" }}>
                 <button className={styles.btn__primary}>Creat new bot</button>
             </Link>
 
-            <Link to="/keyadd">add new key</Link>
+            {userData.wallet_id === null ? (
+                <Link to="/keyadd" className={styles.keyadd_link}>
+                    <PlusIcon className={styles.keyadd_link_icon} />
+                    Add a key
+                </Link>
+            ) : (
+                <Cell title="KEY Settings">
+                    <button className={styles.navButton}>
+                        <div className={styles.content}>
+                            <BinanceIcon />
+                            Binance
+                        </div>
+                        <ArrowRightIcon className={styles.navButton_icon} />
+                    </button>
+                </Cell>
+            )}
 
-            <Cell title="KEY Settings">
-                <button className={styles.navButton}>
-                    <div className={styles.content}>
-                        <BinanceIcon />
-                        Binance
-                    </div>
-                    <ArrowRightIcon className={styles.navButton_icon} />
-                </button>
-            </Cell>
-
-            <Cell title="list of bots">
-                <button className={styles.navButton}>
-                    <div className={styles.content}>
-                        <BtcusdtIcon />
-                        <div className={styles.content_info}>
-                            <div className={styles.content_info_title}>
-                                BTC<span>USDT</span>
+            {userData.bots.length > 0 ? (
+                <Cell title="list of bots">
+                    {userData.bots.map((item: any, index: number) => (
+                        <button className={styles.navButton}>
+                            <div className={styles.content}>
+                                <BtcusdtIcon />
+                                <div className={styles.content_info}>
+                                    <div className={styles.content_info_title}>
+                                        BTC<span>USDT</span>
+                                    </div>
+                                    <div className={styles.content_info_block}>
+                                        TP: 05{" "}
+                                        <span className={styles.circle}></span>
+                                        TP: 05{" "}
+                                        <span className={styles.circle}></span>
+                                        <span className={styles.green}>
+                                            DEP: 70 ₮ 􀄯
+                                        </span>
+                                    </div>
+                                </div>
                             </div>
-                            <div className={styles.content_info_block}>
-                                TP: 05 <span className={styles.circle}></span>
-                                TP: 05 <span className={styles.circle}></span>
-                                <span className={styles.green}>
-                                    DEP: 70 ₮ 􀄯
-                                </span>
+                            <ArrowRightIcon className={styles.navButton_icon} />
+                        </button>
+                    ))}
+                    {/* <button className={styles.navButton}>
+                        <div className={styles.content}>
+                            <BtcusdtIcon />
+                            <div className={styles.content_info}>
+                                <div className={styles.content_info_title}>
+                                    BTC<span>USDT</span>
+                                </div>
+                                <div className={styles.content_info_block}>
+                                    TP: 05{" "}
+                                    <span className={styles.circle}></span>
+                                    TP: 05{" "}
+                                    <span className={styles.circle}></span>
+                                    <span className={styles.green}>
+                                        DEP: 70 ₮ 􀄯
+                                    </span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <ArrowRightIcon className={styles.navButton_icon} />
-                </button>
+                        <ArrowRightIcon className={styles.navButton_icon} />
+                    </button>
 
-                <button className={styles.navButton}>
-                    <div className={styles.content}>
-                        <SolusdcIcon />
-                        <div className={styles.content_info}>
-                            <div className={styles.content_info_title}>
-                                SOL<span>USDC</span>
-                            </div>
-                            <div className={styles.content_info_block}>
-                                TP: 05 <span className={styles.circle}></span>
-                                TP: 05 <span className={styles.circle}></span>
-                                <span className={styles.green}>
-                                    DEP: 234 ₮ 􀄯
-                                </span>
+                    <button className={styles.navButton}>
+                        <div className={styles.content}>
+                            <SolusdcIcon />
+                            <div className={styles.content_info}>
+                                <div className={styles.content_info_title}>
+                                    SOL<span>USDC</span>
+                                </div>
+                                <div className={styles.content_info_block}>
+                                    TP: 05{" "}
+                                    <span className={styles.circle}></span>
+                                    TP: 05{" "}
+                                    <span className={styles.circle}></span>
+                                    <span className={styles.green}>
+                                        DEP: 234 ₮ 􀄯
+                                    </span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <ArrowRightIcon className={styles.navButton_icon} />
-                </button>
+                        <ArrowRightIcon className={styles.navButton_icon} />
+                    </button>
 
-                <button className={styles.navButton}>
-                    <div className={styles.content}>
-                        <TwtusdtIcon />
-                        <div className={styles.content_info}>
-                            <div className={styles.content_info_title}>
-                                TWT<span>USDT</span>
-                            </div>
-                            <div className={styles.content_info_block}>
-                                TP: 08 <span className={styles.circle}></span>
-                                TP: 05 <span className={styles.circle}></span>
-                                <span className={styles.red}>DEP: 432 ₮ 􀄱</span>
+                    <button className={styles.navButton}>
+                        <div className={styles.content}>
+                            <TwtusdtIcon />
+                            <div className={styles.content_info}>
+                                <div className={styles.content_info_title}>
+                                    TWT<span>USDT</span>
+                                </div>
+                                <div className={styles.content_info_block}>
+                                    TP: 08{" "}
+                                    <span className={styles.circle}></span>
+                                    TP: 05{" "}
+                                    <span className={styles.circle}></span>
+                                    <span className={styles.red}>
+                                        DEP: 432 ₮ 􀄱
+                                    </span>
+                                </div>
                             </div>
                         </div>
+                        <ArrowRightIcon className={styles.navButton_icon} />
+                    </button> */}
+                </Cell>
+            ) : (
+                <Cell>
+                    <div className={styles.emptyBots_wrapper}>
+                        <strong className={styles.emptyBots_title}>
+                            You don't have bots yet
+                        </strong>
+                        <p className={styles.emptyBots_paragraph}>
+                            All your bots will be displayed here
+                        </p>
                     </div>
-                    <ArrowRightIcon className={styles.navButton_icon} />
-                </button>
-            </Cell>
+                </Cell>
+            )}
         </div>
     );
 };
