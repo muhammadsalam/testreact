2a14ca22076f8227d1e9cb5ddc29fd24ca2b89fa
Muhammadsalam Musaev <muhammadsalam@list.ru>
2024-02-12 17:35:16 +0300

    bot page

--- src/pages/bot/ui/index.tsx
+++ src/pages/bot/ui/index.tsx
@@ -7,8 +7,6 @@ import { Cell, CellListItem } from "shared/ui";
 import { tgApp } from "shared/lib";
 
 interface BotData {
-    // price_socket_time: 1707476359.3164392;
-    // user_socket_time: null;
     // purchase_price: 0;
     // io_step: 5;
     // take_step: 1;
@@ -19,7 +17,6 @@ interface BotData {
     // type_first_order: "LIMIT";
     // io_step_mrt: 1;
     // take_mrt: 1;
-    // title: "string";
     // price_first_order: 40000;
     // stop_loss: 0;
     // cycles: 1;
@@ -37,6 +34,9 @@ interface BotData {
     // existing_volume: 0;
     // io_count: 3;
     // take_ammount: 50;
+    price_socket_time: null | number;
+    user_socket_time: null | number;
+    title: string;
     orders: {
         pair: string;
         amount_input: number;
@@ -105,16 +105,121 @@ export const BotPage = () => {
         };
     }, []);
 
+    const getDate = (
+        datetime: number | null,
+        key: "price_socket_time" | "user_socket_time"
+    ) => {
+        if (botData) {
+            if (botData[key] && datetime) {
+                const date = new Date(datetime * 1000);
+                return `${date.getDate()}.${date.getMonth() + 1}.${(
+                    date.getFullYear() + ""
+                ).slice(
+                    2
+                )} (${date.getHours()}:${date.getMinutes()}:${date.getMilliseconds()})`;
+            } else return "" + botData[key];
+        }
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.top}>
-                <p className={styles.top_title}>Bot name</p>
+                <p className={styles.top_title}>Bot title - {botData?.title}</p>
                 <p className={styles.top_subtitle}>
                     Some subtitle under title here
                 </p>
             </div>
 
-            <Cell title="orders">
+            <Cell>
+                <CellListItem>
+                    price_socket_time
+                    <span className={styles.black_color}>
+                        {getDate(
+                            botData && botData?.price_socket_time,
+                            "price_socket_time"
+                        )}
+                    </span>
+                </CellListItem>
+                <CellListItem>
+                    user_socket_time
+                    <span className={styles.black_color}>
+                        {getDate(
+                            botData && botData?.user_socket_time,
+                            "user_socket_time"
+                        )}
+                    </span>
+                </CellListItem>
+            </Cell>
+
+            {botData &&
+                botData.orders.map((order, index) => {
+                    return (
+                        <Cell key={index} title={`order ${index + 1}`}>
+                            <CellListItem>
+                                id
+                                <span className={styles.black_color}>
+                                    {order.id}
+                                </span>
+                            </CellListItem>
+                            <CellListItem>
+                                bot_id
+                                <span className={styles.black_color}>
+                                    {order.bot_id}
+                                </span>
+                            </CellListItem>
+                            <CellListItem>
+                                amount_input
+                                <span className={styles.black_color}>
+                                    {order.amount_input}
+                                </span>
+                            </CellListItem>
+                            <CellListItem>
+                                amount_output
+                                <span className={styles.black_color}>
+                                    {order.amount_output}
+                                </span>
+                            </CellListItem>
+                            <CellListItem>
+                                cycle
+                                <span className={styles.black_color}>
+                                    {order.cycle}
+                                </span>
+                            </CellListItem>
+                            <CellListItem>
+                                order_type
+                                <span className={styles.black_color}>
+                                    {order.order_type}
+                                </span>
+                            </CellListItem>
+                            <CellListItem>
+                                pair
+                                <span className={styles.black_color}>
+                                    {order.pair}
+                                </span>
+                            </CellListItem>
+                            <CellListItem>
+                                price
+                                <span className={styles.black_color}>
+                                    {order.price}
+                                </span>
+                            </CellListItem>
+                            <CellListItem>
+                                status
+                                <span className={styles.black_color}>
+                                    {order.status}
+                                </span>
+                            </CellListItem>
+                            <CellListItem>
+                                type
+                                <span className={styles.black_color}>
+                                    {order.type}
+                                </span>
+                            </CellListItem>
+                        </Cell>
+                    );
+                })}
+
+            {/* <Cell title="orders">
                 {botData &&
                     botData.orders.map((order) => {
                         return (
@@ -124,7 +229,7 @@ export const BotPage = () => {
                             </CellListItem>
                         );
                     })}
-            </Cell>
+            </Cell> */}
         </div>
     );
 };
