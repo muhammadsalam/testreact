db6ed1d52eeae660414fcab3cdeb54c2149ce036
Muhammadsalam Musaev <muhammadsalam@list.ru>
2024-06-27 16:18:22 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import Header from "./components/Header/Header";
 
+import { blockVerticalScrollApp } from "./lib/utils/blockVerticalScrollApp";
+
 function App() {
+
+  window.Telegram.WebApp.expand();
+
+  useEffect(() => {
+    blockVerticalScrollApp(true)
+  }, [])
+
   return <>
     <Header />
   </>;
